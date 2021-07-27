d3576f1327f7d0af754a4a92628b7f60721d6c58
Joe Dietrich <joe@joedietrich.net>
2021-07-26 21:16:56 -0400

    Done.

--- index.js
+++ index.js
@@ -1,3 +1,17 @@
 class Formatter {
-  //add static methods here
+  static capitalize(string) {
+    return `${string.charAt(0).toUpperCase()}${string.slice(1)}`;
+  }
+  static sanitize(string) {
+    return string.replace(/[^a-zA-Z0-9\-\' ]/g, '');
+  }
+  static titleize(string) {
+    return string.split(' ').map((word, index) => {
+      const exclusionList = ['a', 'an', 'but', 'of', 'and', 'for', 'at', 'by', 'from', 'the'];
+      if (index !== 0 && exclusionList.includes(word.toLowerCase())) {
+        return word;
+      }
+      return this.capitalize(word);
+    }).join(' ')
+  }
 }
